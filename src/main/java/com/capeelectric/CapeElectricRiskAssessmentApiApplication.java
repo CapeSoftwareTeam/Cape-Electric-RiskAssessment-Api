a7110b5b4a20e0cf8ee25a1a9756dd48438ed02a
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2022-05-06 12:45:08 +0530

    Add files via upload

---
+++ src/main/java/com/capeelectric/CapeElectricRiskAssessmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.capeelectric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapeElectricRiskAssessmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapeElectricRiskAssessmentApiApplication.class, args);
+	}
+
+}
